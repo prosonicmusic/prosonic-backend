ee638711d3f45c2f54d64dcfb1be26a3604f635a
mahmoud <eslamimahmoud98@gmail.com>
2022-08-08 19:25:34 +0430

    custom admin field added for user profile

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
-admin.site.register(UserProfile)
+
+@admin.register(UserProfile)
+class CustomUserProfile(admin.ModelAdmin):
+    list_display = ("user_id", "verified")
