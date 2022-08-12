fd101d8297f1e60ff141ed6f2a3b856f55e0a797
mahmoud <eslamimahmoud98@gmail.com>
2022-08-13 00:28:33 +0430

    DockerFile and docker-compose completed

--- prosonic_backend_core/settings.py
+++ prosonic_backend_core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-9287aq&%+)ov4r)^i*i!v(2@-4$l=%h43t781=a^i5b8w4jkqh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -58,7 +59,7 @@ MIDDLEWARE = [
 
 SWAGGER_SETTINGS = {
     "SECURITY_DEFINITIONS": {
-        "Auth Token eg [Bearer (JWT) ]": {
+        "Auth Token eg [Bearer (JWT)]": {
             "type": "apiKey",
             "name": "Authorization",
             "in": "header",
@@ -151,7 +152,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = "static/"
 
 # Default primary key field type
