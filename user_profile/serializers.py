bc815ba7347160676f6a1f63439b355b8c758b15
mahmoud <eslamimahmoud98@gmail.com>
2022-08-16 18:20:32 +0430

    password validation added

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,12 +1,14 @@
 import email
 from enum import unique
 from itertools import product
+from wsgiref.validate import validator
 from .models import UserProfile
 from prosonic_backend_core import settings
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.auth import get_user_model  # If used custom user model
 from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 UserModel = get_user_model()
 
@@ -40,12 +42,15 @@ class UserRegisterationSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
+    name = serializers.CharField(max_length=254, allow_blank=True, allow_null=True)
+    password = serializers.CharField(validators=[validate_password])
 
     def create(self, validated_data):
 
         user = UserModel.objects.create_user(
             email=validated_data["email"],
             username=validated_data["email"],
+            first_name=validated_data["name"],
         )
         user.set_password(validated_data["password"])
         user.save()
@@ -56,4 +61,4 @@ class UserRegisterationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserModel
-        fields = ("id", "password", "email")
+        fields = ("id", "first_name", "name", "password", "email")
