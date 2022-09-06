2cd9455fb5e498e1b26a4a352ec959d97a37b46d
mahmoud <eslamimahmoud98@gmail.com>
2022-09-06 21:50:39 +0430

    increase THROTTLE_RATES

--- prosonic_backend_core/settings.py
+++ prosonic_backend_core/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "5/day", "user": "5/day"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 
